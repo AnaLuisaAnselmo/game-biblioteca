709136d606c5d772bfa4da23e1b4e78c0f3d312c
sophboo <boonobody0@gmail.com>
2025-08-21 11:45:41 -0300

    3

---
+++ desafio4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fase 4</title>
+</head>
+<body>
+    <h1>FASE 4</h1>
+
+    <div>
+        <h2>Muito bem, agora com o caminho iluminado, por onde você irá para tentar encontrar o livro do seu avô?</h2>
+</div>
+
+    <nav>
+     <input type = "radio" nome = "Fase 4" value="Direita"> (A)Ir pela direita
+     <input type = "radio" nome = "Fase 4" value="Esquerda"> (B)Ir pela Esquerda.
+     <input type = "radio" nome = "Fase 4" value="Fora"> (C)Ir por fora.
+     <input type = "radio" nome = "Fase 4" value="Livros"> (D)Ir pela seção de livros brasileiros.
+           
+     <button>
+        <a href="3">AVANÇAR</a>
+     </button>
+   
+    </nav>
+</body>
+</html>
+
