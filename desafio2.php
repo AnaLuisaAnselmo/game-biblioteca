42b0c3a039252132432175aea90c6ebfcb84e7ca
Ana Luisa Anselmo <analuisaanselmo2008@gmail.com>
2025-08-21 15:18:54 -0300

    concertando a fase 2

--- desafio2.php
+++ desafio2.php
@@ -7,12 +7,17 @@
     <title>FASE 2</title>
 </head>
 <body>
+
+  <h1>FASE 2</h1>
+
+    <div><p><h2>Após entrar na biblioteca abandonada, você percebe que não enxerga nada, você terá que procurar por velas e fósforos. Aonde você vai procurar?</h2></p></div>
+
   <form method="POST">
      <input type = "radio" name = "Fase2" value="americano"> (A)Procurar na seção de livros americanos.
      <input type = "radio" name = "Fase2" value="recepcao"> (B)Procurar na recepção.
      <input type = "radio" name = "Fase2" value="brasileiro"> (C)Procurar na seção de livros brasileiros.
      <input type = "radio" name = "Fase2" value="banheiro"> (D)Procurar no Banheiro.
-     <input type= "submit">
+     <input type= "submit" value="Verificar">
            </form>
 
 
