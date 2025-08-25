c7773439ecd56299617eb09577952ad15588b7f0
Ana Luisa Anselmo <analuisaanselmo2008@gmail.com>
2025-08-25 15:45:11 -0300

    estilização upagina da pontuação

--- paginafinal.php
+++ paginafinal.php
@@ -7,24 +7,31 @@
 </head>
 <link rel="stylesheet" href="paginafinal.css">
 <body>
-    
+
+<div class="php">
  
 <?php
 session_start();
-echo "Pontuação final: " . $_SESSION['pontos'];
+echo "<h1 class='pontuacao'>Pontuação final: " . $_SESSION['pontos'] . "/50 </h1>";
 
 
-?>
+?></div><br><br>
+
+<div class="texto">
 <p>A aventura chegou ao fim, mas a nossa gratidão é só o começo.
 
 Queríamos que você soubesse que cada passo seu nessa jornada, cada escolha, cada desafio superado, foi mais do que apenas um jogo para nós. Foi a prova de que a nossa história, criada com tanto carinho, encontrou seu lugar nos seus pensamentos e na sua dedicação.
 
-Obrigado por nos dar a chance de compartilhar essa paixão. O apoio de cada um de vocês faz o nosso trabalho ter sentido e nos inspira a continuar sonhando e criando.</p>
-<br><p>
+Obrigado por nos dar a chance de compartilhar essa paixão. O apoio de cada um de vocês faz o nosso trabalho ter sentido e nos inspira a continuar sonhando e criando.</p></div><br>
+
+<div class="agradecimentos">
+<p>
 Do fundo do coração, 
 <div class="glitch">GLITCH</div></p>
-<p>Obrigado por jogar!</p>
+
+<p>Obrigado por jogar!</p></div>
 <a href="creditos.html">Créditos</a>
+
 
 <form method="POST">
    <input type='submit' value='Jogar novamente'>
