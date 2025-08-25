2cb4d9771cd4bc715953e78d137a8d53d83990b4
Ana Luisa Anselmo <analuisaanselmo2008@gmail.com>
2025-08-25 07:23:39 -0300

    ageitando o desafio 2

--- desafio2_be.php
+++ desafio2_be.php
@@ -2,10 +2,10 @@
 class Desafio2_be{
     public function Resposta2($resposta){
         if($resposta == "americano"){
-            echo "Você acertou. <a href='desafio3.php'>Prossiga!</a>";
+            echo "Você encontrou as velas, procure os fósforos. <a href='desafio3.php'>Prossiga!</a>";
         }
         if($resposta == "recepcao"){
-            echo "Você só encontrou a recepcionista, tente outra alternativa!";
+            echo "Você não encontrou nada, tente outra alternativa!";
         }
         if($resposta == "brasileiro"){
         echo "Você só perdeu tempo, tente outra alternativa!";
