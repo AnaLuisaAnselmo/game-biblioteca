1be6c1e0d2dfd09b666ec8a5c1743d376c4b2d9e
amandal28 <amandaloliveira28@gmail.com>
2025-08-25 07:55:57 -0300

    desafio2 css

--- desafio2.php
+++ desafio2.php
@@ -3,40 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="desafio2.css">
 
     <title>FASE 2</title>
 </head>
 <body>
 
+<div class="container">
   <h1>FASE 2</h1>
 
-    <div><p><h2>Após entrar na biblioteca abandonada, você percebe que não enxerga nada, você terá que procurar por velas e fósforos. Aonde você vai procurar?</h2></p></div>
+    <div class="caixa-pergunta">
+      <p class="pergunta">
+        Após entrar na biblioteca abandonada, você percebe que não enxerga nada, você terá que procurar por velas e fósforos. Aonde você vai procurar?</p>
+        <small>*Para ir para a próxima fase, basta escolher uma das opções abaixo e verifica-la.</small>
 
-  <form method="POST">
-     <input type = "radio" name = "Fase2" value="americano"> (A)Procurar na seção de livros americanos.
-     <input type = "radio" name = "Fase2" value="recepcao"> (B)Procurar na recepção.
-     <input type = "radio" name = "Fase2" value="brasileiro"> (C)Procurar na seção de livros brasileiros.
-     <input type = "radio" name = "Fase2" value="banheiro"> (D)Procurar no Banheiro.
-     <input type= "submit" value="Verificar">
+  <form method="POST" class="form-fase">
+    <label class="opcao">
+     <input type = "radio" name = "Fase2" value="americano"> (A) Procurar na seção de livros americanos.
+     </label>
+     <label class="opcao">
+     <input type = "radio" name = "Fase2" value="recepcao"> (B) Procurar na recepção.
+      </label>
+      <label class="opcao">
+     <input type = "radio" name = "Fase2" value="brasileiro"> (C) Procurar na seção de livros brasileiros.
+      </label>
+      <label class="opcao">
+     <input type = "radio" name = "Fase2" value="banheiro"> (D) Procurar no Banheiro.
+      </label>
+
+      <button type="submit" class="btn">Verificar</button>
            </form>
+</div>
 
-
-</body>
-</html>
-
+<div class="resposta">
 <?php
 require_once "desafio2_be.php";
-
-$geral = new desafio2_be();
-
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
+      $geral = new desafio2_be();
+      $resposta = $_POST['Fase2'];
+      $geral->Resposta2($resposta);
 
-        $resposta = $_POST['Fase2'];
-
-       
-        $geral->Resposta2($resposta);
-
-}
-       
-     
-  
+} ?>
+</div>       
+ </div>    
+</body>
+</html>  
