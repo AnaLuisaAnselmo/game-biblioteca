79b08fe8e055d7974805150977ca802b8e91ed93
WPOTC <gutomergr.s@gmail.com>
2025-08-25 11:54:03 -0300

    oie

--- desafio2_be.php
+++ desafio2_be.php
@@ -2,15 +2,19 @@
 class Desafio2_be{
     public function Resposta2($resposta){
         if($resposta == "americano"){
+            $_SESSION['pontos']+= 5;
             echo "Você encontrou as velas, procure os fósforos. <a href='desafio3.php'>Prossiga!</a>";
         }
         if($resposta == "recepcao"){
+            $_SESSION['pontos']-= 2;
             echo "Você não encontrou nada, tente outra alternativa!";
         }
         if($resposta == "brasileiro"){
+            $_SESSION['pontos']-= 2;
         echo "Você só perdeu tempo, tente outra alternativa!";
         }
          if($resposta == "banheiro"){
+            $_SESSION['pontos']-= 2;
         echo "Você só encontrou um fedor tremendo, tente outra alternativa!";
         }
 }
